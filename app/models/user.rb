f5ff310c8f679d7026d3d29ffbf96009524a622f
hellfishg <hellfish.g@protonmail.com>
2019-08-23 19:03:33 -0300

    Fix user trakable remove

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
-  # validates :first_name, :last_name, presence: true
 end
