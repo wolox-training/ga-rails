1f1db200b8f0cc882771c8f65c2734acad9effdb
hellfishg <hellfish.g@protonmail.com>
2019-09-02 12:58:56 -0300

    Fixed facatori book validation title

--- db/migrate/20190902144027_change_book_title_max_characters.rb
+++ db/migrate/20190902144027_change_book_title_max_characters.rb
@@ -1,5 +1,5 @@
 class ChangeBookTitleMaxCharacters < ActiveRecord::Migration[6.0]
   def change
-    change_column :books, :title, :string, :limit => 25
+    change_column :books, :title, :string, limit: 25
   end
 end
