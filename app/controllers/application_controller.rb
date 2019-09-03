7702277ae915786e5f574adf37f543ff6b16646e
hellfishg <hellfish.g@protonmail.com>
2019-09-03 12:25:46 -0300

    Add secret key verification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery with: :null_session
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,4 @@
 class BooksController < ApplicationController
-  before_action :authenticate_user!
-
   def show
     render json: Book.find(book_id), serializer: BookSerializer
   end
