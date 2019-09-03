d5f8b9ce457767280fa90206735fd07fa490272e
hellfishg <hellfish.g@protonmail.com>
2019-09-03 12:31:28 -0300

    Fix rubocop offences

--- spec/controllers/book_controller_spec.rb
+++ spec/controllers/book_controller_spec.rb
@@ -3,5 +3,4 @@ require 'rails_helper'
 RSpec.describe BooksController, type: :controller do
   it { should route(:get, '/books').to(action: :index) }
   it { should route(:get, '/books/3').to(action: :show, id: 3) }
-
 end
