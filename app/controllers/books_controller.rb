5c08792c962161f5b62367170d9ebb35274f01fb
hellfishg <hellfish.g@protonmail.com>
2019-09-03 14:39:24 -0300

    Private method book_id for params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,17 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
 
-  def book_id
-    params.require(:id)
-  end
-
   def show
     render json: Book.find(book_id), serializer: BookSerializer
   end
 
   def index
     render json: Book.all, each_serializer: BookSerializer
+  end
+
+  private
+
+  def book_id
+    params.require(:id)
   end
 end
