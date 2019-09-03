cb52954c3fc73fcd4c559d47b2a26879f93b210e
hellfishg <hellfish.g@protonmail.com>
2019-09-03 16:07:44 -0300

    Test status code

--- spec/controllers/book_controller_spec.rb
+++ spec/controllers/book_controller_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe BooksController, type: :controller do
+
+  it 'responds with 200 status' do
+    expect(response).to have_http_status(:ok)
+  end
+
   it { should route(:get, '/books').to(action: :index) }
   it { should route(:get, '/books/3').to(action: :show, id: 3) }
 end
