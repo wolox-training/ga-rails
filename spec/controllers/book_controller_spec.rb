bf8c0fc8cee780b731db73689f47f1a41a5683a4
hellfishg <hellfish.g@protonmail.com>
2019-09-05 14:49:27 -0300

    beautify rspec test

--- spec/controllers/book_controller_spec.rb
+++ spec/controllers/book_controller_spec.rb
@@ -1,29 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe BooksController do
-  include Wor::Paginate
+  describe 'GET #index' do
+    context 'GET routes checks' do
+      it { should route(:get, '/books').to(action: :index) }
+      it { should route(:get, '/books/1').to(action: :show, id: 1) }
+    end
 
-  describe 'GET rutes checks' do
-    it { should route(:get, '/books').to(action: :index) }
-    it { should route(:get, '/books/1').to(action: :show, id: 1) }
-  end
+    context 'without authenticated user' do
+      subject(:http_request) { get :index }
+      it { should have_http_status(:unauthorized) }
+    end
 
-  describe 'GET #index without authenticated user' do
-    subject(:http_request) { get :index }
-    it { should have_http_status(:unauthorized) }
-  end
-
-  describe 'GET #index with authenticated user' do
     context 'When you are looking all books' do
-      include_context 'Authenticated User'
+      include_context 'with authenticated user'
       let!(:books) { create_list(:book, 3) }
       subject!(:http_response) { get :index }
 
-      it 'responds with a paginated json' do
+      it 'with a paginated json' do
         expect(JSON.parse(http_response.body)).to be_paginated
       end
 
-      it 'responses with books json' do
+      it 'with books answers in json' do
         expected = ActiveModel::Serializer::CollectionSerializer.new(
           books, each_serializer: BookSerializer
         ).to_json
@@ -35,18 +33,17 @@ RSpec.describe BooksController do
     end
   end
 
-  describe 'GET #show without authenticated user' do
-    subject(:http_request) { get :index }
-    it { should have_http_status(:unauthorized) }
-  end
-
-  describe 'GET #show with authenticated user' do
+  describe 'GET #show' do
+    context 'without authenticated user' do
+      subject(:http_request) { get :index }
+      it { should have_http_status(:unauthorized) }
+    end
     context 'When you look a book with correct id' do
-      include_context 'Authenticated User'
+      include_context 'with authenticated user'
       let!(:book) { create(:book) }
       subject!(:http_response) { get :show, params: { id: book.id } }
 
-      it 'responses with json' do
+      it 'with answers in json' do
         expect(JSON.parse(http_response.body).to_json).to eq BookSerializer.new(book).to_json
       end
 
