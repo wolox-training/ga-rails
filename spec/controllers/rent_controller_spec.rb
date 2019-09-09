0f2a2310f2b9ca46c12515f629f3f365f46b511e
hellfishg <hellfish.g@protonmail.com>
2019-09-09 17:18:16 -0300

    Create test POST #create for rents

--- spec/controllers/rent_controller_spec.rb
+++ spec/controllers/rent_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe RentsController do
 <<<<<<< HEAD
+<<<<<<< HEAD
   describe 'POST #create' do
     context 'When create rent' do
       include_context 'with authenticated user'
@@ -23,10 +24,31 @@ RSpec.describe RentsController do
       end
 
       it { is_expected.to have_http_status(201) }
+=======
+  describe 'POST #create' do
+    context 'without authenticated user' do
+      subject(:http_request) { post :create }
+      it { should have_http_status(:unauthorized) }
+    end
+
+    context 'When create rent' do
+      include_context 'with authenticated user'
+      let!(:rent) { create(:rent) }
+      subject!(:http_response) do
+        post :create, params: {
+          user: rent.user, book: rent.book
+        }
+      end
+
+      it 'with a paginated json' do
+        expect(JSON.parse(http_response.body).to_json).to eq RentSerializer.new(rent).to_json
+      end
+>>>>>>> 65452d5... Create test POST #create for rents
     end
   end
 
   describe 'GET #index' do
+<<<<<<< HEAD
     context 'When you look a book with correct id' do
       include_context 'with authenticated user'
       let(:rent) { create(:rent) }
@@ -41,4 +63,25 @@ RSpec.describe RentsController do
   end
 =======
 >>>>>>> 76f7f49... Prototype rents routes.
+=======
+    context 'GET routes checks' do
+      it { should route(:get, '/rents').to(action: :index) }
+    end
+
+    context 'without authenticated user' do
+      subject(:http_request) { get :index }
+      it { should have_http_status(:unauthorized) }
+    end
+
+    context 'When you look a book with correct id' do
+      include_context 'with authenticated user'
+      let!(:rent) { create(:rent) }
+      # subject!(:http_response) { get :index, params: { id: 1} }
+
+      it 'with answers in json' do
+        # expect(JSON.parse(http_response.body).to_json).to eq RentSerializer.new(rent).to_json
+      end
+    end
+  end
+>>>>>>> 65452d5... Create test POST #create for rents
 end
