cd5577cc84dbc87bb98e3a08c16e592d0cdbe41a
hellfishg <hellfish.g@protonmail.com>
2019-09-25 18:05:10 -0300

    Fixed for PR1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   include Pundit
   include Wor::Paginate
-  include Sidekiq::Worker
   before_action :authenticate_user!, unless: :devise_controller?
   include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery with: :null_session
--- app/mailers/rent_mailer.rb
+++ app/mailers/rent_mailer.rb
@@ -6,16 +6,6 @@ class RentMailer < ApplicationMailer
     @book = @rent.book
     user = @rent.user
     mail(to: user.email,
-         subject: "#{@book.title} rented!",
-         body: body_format)
-  end
-
-  private
-
-  def body_format
-    body_text = "RENT DETAILS:\n\n"
-    body_text << "Title: #{@book.title}, genre: #{@book.genre}, Author: #{@book.author}\n"
-    body_text << "Date of creation of the rent: #{@rent.rent_in}\n"
-    body_text << "End of rental: #{@rent.rent_out}"
+         subject: "#{@book.title} rented!")
   end
 end
