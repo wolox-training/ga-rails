2a12be2fbb8cb43bc1ecc2730fdd185c9fd4a672
hellfishg <hellfish.g@protonmail.com>
2019-09-26 15:52:03 -0300

    Test with errors

--- app/mailers/rent_mailer.rb
+++ app/mailers/rent_mailer.rb
@@ -1,9 +1,22 @@
+# require 'i18n'
+# I18n.locale = I18n.default_locale
+
 class RentMailer < ApplicationMailer
   default from: 'notifications@wolox.com.ar'
 
   def send_create_rent
+    # I18n.locale = I18n.default_locale
+    # set_locale
     @rent = Rent.find(params[:rent_id])
     @book = @rent.book
+    pepe = I18n.t(:book_details)
     mail(to: @rent.user.email, subject: "#{@book.title} rented!")
+
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = :es
   end
 end
