af1c443cf72c79bececd8398f34f0b8650157a01
hellfishg <hellfish.g@protonmail.com>
2019-09-30 19:51:19 -0300

    Book_suggestion route

--- app/models/book_suggestion.rb
+++ app/models/book_suggestion.rb
@@ -1,6 +1,5 @@
 class BookSuggestion < ApplicationRecord
   belongs_to :user, optional: true
 
-  validates :author, :title, :link, :editor, :year,
-            presence: true
+  validates :synopsis, :price, :author, :title, :link, :editor, :year, presence: true
 end
