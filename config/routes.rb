b689d257dcc87df35297fe903d333956d7ee50a2
hellfishg <hellfish.g@protonmail.com>
2019-09-30 16:03:35 -0300

    rails_admin install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :books, only: %i[index show] do
     collection do
       get :book_information
